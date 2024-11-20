39b71651bc9bdbc4922ef25aad7811eba8db74d6
gnikhil98 <153263393+gnikhil98@users.noreply.github.com>
2024-11-20 12:26:22 +0530

    new access key test

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "nexttestaccess": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
